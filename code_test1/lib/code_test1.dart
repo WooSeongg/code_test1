d6404a638a321214be7ecb8949d26f4c3f270253
choiws <choiws@smsoftlab.co.kr>
2024-09-06 20:30:06 +0900

    first commit

---
+++ code_test1/lib/code_test1.dart
@@ -0,0 +1,14 @@
+library code_test1;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+class A {
+
+  void printName() {
+    print("myName is class A");
+  }
+}
